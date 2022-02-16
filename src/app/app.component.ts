f4e0f777cd855342df588addd832fb998b0ec7c9
ahlem-17 <ahlem.mh2020@icloud.com>
2022-02-16 14:23:10 +0200

    lisätän tiedostot

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {COURSES} from '../db-data';
+import { Course } from './model/course';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  courses = COURSES;
+  title = COURSES[0].description;
+  price = 9.99;
+  rate = 0.67;
+
+  course = COURSES[0];
+  startDate = new Date(2000, 0 , 1);
+
+  onCouseSelected(course: Course) {
+    
+
+  }
+
+}
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+
+  @Input() 
+  course: Course;
+
+  @Input()
+  cardIndex: number;
+  @Output('courseSelected')
+  courseEmitter = new EventEmitter<Course>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onCourseViewed() {
+    this.courseEmitter.emit(this.course);
+  }
+  isImageVisible() {
+    return this.course && this.course.iconUrl;
+  }
+  cardClasses() {
+    if (this.course.category == 'BEGINNER') {
+      return 'beginner';
+
+    }
+  }
+
+  cardStyles() {
+    return {'background-image': 'url(' + this.course.iconUrl + ')'};
+  }
+
+}
